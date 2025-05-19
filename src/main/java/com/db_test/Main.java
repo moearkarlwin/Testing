1d6747795f5df0d16a3893c8761517e002d7fc8d
moearkarlwin <moearkarlwin@gmail.com>
2025-05-19 16:12:20 +0630

    Database connection.

--- src/main/java/com/db_test/Main.java
+++ src/main/java/com/db_test/Main.java
@@ -1,8 +1,42 @@
 package com.db_test;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Main {
 
+    private String ip = "localhost";
+    private String port = "3306";
+    private String dbname = "test_db";
+    private String username = "root";
+    private String password = "";
+
+    protected Connection get_Db_Connection() {
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://" + ip + ":" + port + "/" + dbname,
+                    username, password);
+            System.out.println("Successful Connection.");
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return con;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Main m = new Main();
+        Connection con = m.get_Db_Connection();
+
+        try {
+            if(con != null) {
+                con.close();
+                System.out.println("Connection is closed.");
+            }
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
